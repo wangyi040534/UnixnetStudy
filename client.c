42d1d08ea6907fc2ccbd857499b1f2cae58f700d
wangyi040534 <wy040534@163.com>
2015-03-23 19:05:57 +0800

    15.03.23

--- client.c
+++ client.c
@@ -11,19 +11,24 @@ int main(int argc, char **argv)
 	int pid;
 	int n;
 	char request[MAXN], reply[MAXN];
-	if(argc != 6)
+	if(argc != 4)
 		printf("usgae:client<hostname or IPaddr><port><#children>"
-				"<#loops/child><#bytes/request>");
-	nchildren = atoi(argv[3]);
-	nloops = atoi(argv[4]);
-	nbytes = atoi(argv[5]);
-	printf(request, sizeof(request), "d%\n", nbytes);
-
+				"<#loops/child><#bytes/request>\n");
+	//nchildren = atoi(argv[3]);
+	nchildren = 10;
+	//nloops = atoi(argv[4]);
+	//nbytes = atoi(argv[5]);
+	nloops = 10;
+	nbytes = atoi(argv[3]);
+    snprintf(request, sizeof(request), "%d\n", nbytes);
+	//printf("initialize successfully!\n");
 	i = 0;
 	for(i; i < nchildren; i++){
 		if((pid = fork()) == 0){
+            printf("%d child fork successfully!\n",i);
 			j = 0;
 			for(j;j < nloops; j++){
+				printf("%d child %d loop is successful!\n", i, j);
 				fd = tcp_connect(argv[1], argv[2]);
 				write(fd, request, strlen(request));
 				if((n = read(fd, reply, nbytes)) != nbytes)
@@ -43,14 +48,17 @@ int main(int argc, char **argv)
 
 int tcp_connect(char *ip, char *port)
 {
-
+    //printf("not connected\n");
 	int sockfd, connfd;
 	struct sockaddr_in  servaddr;
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(atoi(port));
-	inet_pton(AF_INET,*ip, &servaddr.sin_addr);
-	connfd = connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+	inet_pton(AF_INET,ip, &servaddr.sin_addr);
+	if((connfd = connect(sockfd, (struct sockaddr*)&servaddr,
+				        sizeof(servaddr))) != 0)
+		printf("connect failed!\n");
+    //printf("%d is connected",connfd);
 	return connfd;
 }
